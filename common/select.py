994b332c26c924a0806b71ade789ccb56617d157
kitsamho <kitsamho@gmail.com>
2023-06-04 19:47:13 +0100

    updated docstrings and type hints

--- common/select.py
+++ common/select.py
@@ -1,25 +1,70 @@
+import pandas as pd
 from collections import defaultdict
+from typing import List, Union, Optional
 
 
-def mask_range(df, col, start, end):
+def mask_range(df: pd.DataFrame, col: str, start: Union[int, float], end: Union[int, float]) -> pd.DataFrame:
+    """
+    Filter rows of a DataFrame based on a range of values in a specific column.
+
+    Args:
+        df: The DataFrame to filter.
+        col: The name of the column to filter on.
+        start: The lower bound of the range.
+        end: The upper bound of the range.
+
+    Returns:
+        The filtered DataFrame.
+    """
     return df[(df[col] >= start) & (df[col] <= end)]
 
 
-def mask_value(df, col, value):
+def mask_value(df: pd.DataFrame, col: str, value: Union[int, float]) -> pd.DataFrame:
+    """
+    Filter rows of a DataFrame based on a specific value in a specific column.
+
+    Args:
+        df: The DataFrame to filter.
+        col: The name of the column to filter on.
+        value: The value to filter for.
+
+    Returns:
+        The filtered DataFrame.
+    """
     return df[df[col] == value]
 
 
-def get_min_max_values(df, col, number_type):
+def get_min_max_values(df: pd.DataFrame, col: str, number_type: type) -> tuple:
+    """
+    Get the minimum and maximum values from a specific column of a DataFrame.
+
+    Args:
+        df: The DataFrame to analyze.
+        col: The name of the column.
+        number_type: The type of the column (e.g., int, float).
+
+    Returns:
+        A tuple containing the minimum and maximum values.
+    """
     min_value, max_value = number_type(df[col].min()), number_type(df[col].max())
     return min_value, max_value
 
 
-def select_movie_data(df_movies,year_start, year_end=None,
-                       # revenue_low=None, revenue_high=None,
-                       # budget_low=None, budget_high=None,
-                       vote_low=None, vote_high=None):
+def select_movie_data(df_movies: pd.DataFrame, year_start: Optional[int] = None, year_end: Optional[int] = None,
+                      vote_low: Optional[Union[int, float]] = None, vote_high: Optional[Union[int, float]] = None) -> pd.DataFrame:
+    """
+    Select movie data from a DataFrame based on specified criteria.
 
+    Args:
+        df_movies: The DataFrame containing movie data.
+        year_start: The start year of the movies.
+        year_end: The end year of the movies.
+        vote_low: The lower bound of the movie vote average.
+        vote_high: The upper bound of the movie vote average.
 
+    Returns:
+        The filtered DataFrame.
+    """
     if year_start:
         df_movies = mask_range(df_movies, 'm_release_year', year_start, year_end)
     if vote_low:
@@ -28,7 +73,16 @@ def select_movie_data(df_movies,year_start, year_end=None,
     return df_movies
 
 
-def select_gender(gender_choice):
+def select_gender(gender_choice: str) -> Union[float, None]:
+    """
+    Select the gender value based on the provided choice.
+
+    Args:
+        gender_choice: The selected gender choice.
+
+    Returns:
+        The gender value (1.0 for female, 2.0 for male) or None if "Everyone" is chosen.
+    """
     if gender_choice == 'Everyone':
         gender_mask = None
     elif gender_choice == 'Male':
@@ -38,11 +92,20 @@ def select_gender(gender_choice):
     return gender_mask
 
 
-def select_cast_data(df_cast,
-                      popularity_low=None, popularity_high=None,
-                      gender=None,
-                      adult=None):
+def select_cast_data(df_cast: pd.DataFrame, popularity_low: Optional[Union[int, float]] = None,
+                     popularity_high: Optional[Union[int, float]] = None, gender: Optional[float] = None) -> pd.DataFrame:
+    """
+    Select cast data from a DataFrame based on specified criteria.
 
+    Args:
+        df_cast: The DataFrame containing cast data.
+        popularity_low: The lower bound of the cast popularity.
+        popularity_high: The upper bound of the cast popularity.
+        gender: The selected gender.
+
+    Returns:
+        The filtered DataFrame.
+    """
     if popularity_low:
         df_cast = mask_range(df_cast, 'c_popularity', popularity_low, popularity_high)
     if gender:
@@ -50,13 +113,33 @@ def select_cast_data(df_cast,
     return df_cast
 
 
-def mask_on_actor_edge_frequency(df_d3, edge_frequency_dict, min_threshold):
+def mask_on_actor_edge_frequency(df_d3: pd.DataFrame, edge_frequency_dict: dict, min_threshold: int) -> pd.DataFrame:
+    """
+    Filter edges in a DataFrame based on the frequency of the actors in the edge.
+
+    Args:
+        df_d3: The DataFrame containing edge data.
+        edge_frequency_dict: A dictionary containing actor frequencies.
+        min_threshold: The minimum threshold for actor frequency.
+
+    Returns:
+        The filtered DataFrame.
+    """
     actors_to_mask = [k for k, v in edge_frequency_dict.items() if v > min_threshold]
     mask = df_d3['source'].isin(actors_to_mask) & df_d3['target'].isin(actors_to_mask)
     return df_d3[mask].reset_index(drop=True)
 
 
-def get_actor_co_star_dict(df_d3_masked):
+def get_actor_co_star_dict(df_d3_masked: pd.DataFrame) -> dict:
+    """
+    Get a dictionary of actors and their co-stars from a DataFrame of masked edges.
+
+    Args:
+        df_d3_masked: The DataFrame containing masked edge data.
+
+    Returns:
+        A dictionary mapping actors to their co-stars.
+    """
     dic_merged = defaultdict(list)
 
     for source, target in zip(df_d3_masked['source'], df_d3_masked['target']):
@@ -67,19 +150,38 @@ def get_actor_co_star_dict(df_d3_masked):
     return dic_source_target
 
 
-def find_common_movies(df, actor1, actor2):
+def find_common_movies(df: pd.DataFrame, actor1: str, actor2: str) -> List[str]:
+    """
+    Find common movies between two actors in a DataFrame.
+
+    Args:
+        df: The DataFrame containing actor and movie data.
+        actor1: The name of the first actor.
+        actor2: The name of the second actor.
+
+    Returns:
+        A list of common movie names.
+    """
     actor1_movies = set(df[df['c_name'] == actor1]['m_movie'])
     actor2_movies = set(df[df['c_name'] == actor2]['m_movie'])
     common_movies = actor1_movies.intersection(actor2_movies)
     return list(common_movies)
 
 
-def get_poster_paths(df, common_movies, imdb_image_path):
+def get_poster_paths(df: pd.DataFrame, common_movies: List[str], imdb_image_path: str) -> List[tuple]:
+    """
+    Get the poster paths for common movies from a DataFrame.
+
+    Args:
+        df: The DataFrame containing movie data.
+        common_movies: A list of common movie names.
+        imdb_image_path: The base URL for the movie poster images.
+
+    Returns:
+        A list of tuples containing the movie name and its corresponding poster path.
+    """
     res = []
     for movie in common_movies:
         poster_path = df[df['m_movie'] == movie]['m_poster_path'].unique()[0]
-        res.append((movie, imdb_image_path+poster_path))
+        res.append((movie, imdb_image_path + poster_path))
     return res
-
-
-
