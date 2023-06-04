b81635fcc53ef5589c6d6a56b7ae1df71e7b8413
kitsamho <kitsamho@gmail.com>
2023-06-04 19:49:09 +0100

    updated docstrings and type hints

--- common/show.py
+++ common/show.py
@@ -1,27 +1,47 @@
+import pandas as pd
 import streamlit as st
 import altair as alt
 from d3blocks import D3Blocks
+from typing import List, Tuple
 
-def cache_d3_network_plot(df_d3_masked,
-                           file_path,
-                           edge_distance=100,
-                           node_size=4,
-                           fontsize=8):
 
+def cache_d3_network_plot(df_d3_masked: pd.DataFrame, file_path: str, edge_distance: int = 100,
+                          node_size: int = 4, fontsize: int = 8) -> None:
+    """
+    Caches a D3 network plot to a file.
+
+    Args:
+        df_d3_masked: The DataFrame containing masked edge data.
+        file_path: The path to save the cached plot file.
+        edge_distance: The distance between nodes in the plot.
+        node_size: The size of the nodes in the plot.
+        fontsize: The font size of the nodes in the plot.
+    """
     d3 = D3Blocks()
     d3.d3graph(df_d3_masked)
     d3.D3graph.set_edge_properties(directed=False, edge_distance=edge_distance)
     d3.D3graph.set_node_properties(color='cluster', size=node_size, edge_size=0.2, fontsize=fontsize)
     d3.D3graph.show(filepath=file_path)
-    return
 
 
-def plot_html(cached_html, image_size=(950, 550)):
+def plot_html(cached_html: str, image_size: Tuple[int, int] = (950, 550)) -> None:
+    """
+    Displays a cached HTML plot in Streamlit.
+
+    Args:
+        cached_html: The path to the cached HTML plot file.
+        image_size: The size of the displayed image in Streamlit.
+    """
     st.components.v1.html(cached_html, width=image_size[0], height=image_size[1])
-    return
 
 
-def display_image_grid(paths):
+def display_image_grid(paths: List[Tuple[str, str]]) -> None:
+    """
+    Displays a grid of images in Streamlit.
+
+    Args:
+        paths: A list of tuples containing the image caption and path.
+    """
     num_images = len(paths)
 
     # Calculate the grid size based on the number of images
@@ -43,20 +63,21 @@ def display_image_grid(paths):
                 break
 
 
-def plot_graph_metrics(df, x_axis, y_axis, size=40, height=800, width=800):
+def plot_graph_metrics(df: pd.DataFrame, x_axis: str, y_axis: str, size: int = 40,
+                       height: int = 800, width: int = 800) -> alt.Chart:
     """
     Plots a scatter plot to visualize the graph metrics.
 
     Args:
-        df (pandas.DataFrame): DataFrame containing the graph metrics.
-        x_axis (str): Name of the column representing the x-axis.
-        y_axis (str): Name of the column representing the y-axis.
-        size (int, optional): Size of the markers in the scatter plot. Default is 40.
-        height (int, optional): Height of the plot in pixels. Default is 800.
-        width (int, optional): Width of the plot in pixels. Default is 800.
+        df: DataFrame containing the graph metrics.
+        x_axis: Name of the column representing the x-axis.
+        y_axis: Name of the column representing the y-axis.
+        size: Size of the markers in the scatter plot. Default is 40.
+        height: Height of the plot in pixels. Default is 800.
+        width: Width of the plot in pixels. Default is 800.
 
     Returns:
-        altair.Chart: The scatter plot visualization.
+        The scatter plot visualization as an altair.Chart object.
     """
     marker_chart = alt.Chart(df).mark_circle(size=size).encode(
         x=x_axis,
@@ -68,7 +89,3 @@ def plot_graph_metrics(df, x_axis, y_axis, size=40, height=800, width=800):
     )
 
     return marker_chart
-
-
-
-
